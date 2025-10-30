4fefd22034894beb0d0a82f798f86d91d8f99243
sampathreddygoluguri <146423494+sampathreddygoluguri@users.noreply.github.com>
2025-10-30 17:00:14 +0530

    Implement basic arithmetic operations in Java

---
+++ Day-1/question1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // TODO: Write your code here
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        int S = num1 + num2;
+        int D = num1 - num2;
+        int P = num1 * num2;
+        int Q = 0, R = 0;
+        if(num2 != 0){
+            Q = num1 / num2;
+            R = num1 % num2;
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+        
+        System.out.println("Sum: " + S);
+        System.out.println("Difference: " + D);
+        System.out.println("Product: " + P);
+        System.out.println("Quotient: " + Q);
+        System.out.println("Remainder: " + R);
+
+        scanner.close();
+    }
+}
